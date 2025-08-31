9913ef843f06329724ef0c1d0fc392005e6bfcf9
craftcoderealm-maker <craftcoderealm@gmail.com>
2025-08-31 18:40:27 +0500

    Class 1

---
+++ Basics Of Python/C1_python_fundamentals.py
@@ -0,0 +1,17 @@
+# Python is a case sensitive language
+
+# 1. Comments
+
+# 2. Python Output
+
+# 3. Data Types
+
+# 4. Variables
+
+# 5. Keywords & Identifiers
+
+# 6. User Input
+
+# 7. Type Conversion
+
+# 8. Literals
---
+++ Basics Of Python/C1_tasks.py
@@ -0,0 +1,67 @@
+# Task : Session 1
+# Solve these questions own your own and try to test yourself what you have learned in the session.
+
+# Happy Learning!
+
+# ---------
+# ---------
+# ---------
+
+# Q1 :- Print the given strings as per stated format.
+
+# Given strings:
+
+# ```
+# "Data" "Science" "Mentorship" "Program"
+# "By" "CampusX"
+# ```
+
+# Output:
+# ```
+# Data-Science-Mentorship-Program-started-By-CampusX
+# ```
+
+# Concept- [Seperator and End]
+
+# ---------
+# ---------
+# ---------
+
+# Q2:- Write a program that will convert celsius value to fahrenheit.
+
+# ---------
+# ---------
+# ---------
+
+# Q3:- Take 2 numbers as input from the user.Write a program to swap the numbers without using any special python syntax.
+
+# ---------
+# ---------
+# ---------
+
+# Q4:- Write a program to find the euclidean distance between two coordinates.Take both the coordinates from the user as input.
+
+# ---------
+# ---------
+# ---------
+
+# Q5:- Write a program to find the simple interest when the value of principle,rate of interest and time period is provided by the user.
+
+# ---------
+# ---------
+# ---------
+
+# Q6:- Write a program that will tell the number of dogs and chicken are there when the user will provide the value of total heads and legs.
+
+# For example:
+# Input:
+# heads -> 4
+# legs -> 12
+# Output:
+# dogs -> 2
+# chicken -> 2
+
+# ---------
+# ---------
+# ---------
+
